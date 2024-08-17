4cdce2bf6e32b353be3a347590e41e8b6c610aa8
Muhammad Awais Qarni <owaisq2019@gmail.com>
2024-08-17 15:25:00 +0500

    image uploading

--- class_03/next.config.mjs
+++ class_03/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        // domains:[]
+        remotePatterns:[
+            {
+                protocol:"https",
+                hostname:"images.pexels.com"
+            }
+        ]
+    },
+};
 
 export default nextConfig;
---
+++ class_03/src/app/imagehandle/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+import houseimage from '@/Assets/house.jpeg'
+export default function imagehandle() {
+  return (
+    <div>
+     <Image 
+     src={"https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"} 
+     height={100} width={100} alt='image'/>
+    {/* <Image src={houseimage}
+     height={100} width={100} alt='image'/> */}
+     {/* <Image src={"/house.jpeg"}  height={200} width={200} alt='house'/> */}
+    </div>
+  )
+}
